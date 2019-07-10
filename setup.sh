0a2645d07def8cd46b1f9e2ad57f71cccb9e14a6
Tom Pridham <pridham.tom@gmail.com>
2019-07-09 20:53:41 -0600

    add vim setup and coc install steps, replace ~ with $HOME

--- setup.sh
+++ setup.sh
@@ -11,26 +11,29 @@ git config --global user.email "pridham.tom@gmail.com"
 git config --global user.name "Tom Pridham"
 git config --global core.editor "vim"
 
-#cd ~/
+#cd $HOME/
 #mkdir projects
 #cd projects
 #git clone git@github.com:TomPridham/tomfig.git
-mkdir ~/.config/autostart
-mkdir ~/.vim
-ln -s ~/projects/tomfig/.bash ~/.bash
-ln -s ~/projects/tomfig/.bashrc ~/.bashrc
-ln -s ~/projects/tomfig/.bashrc ~/.bash_profile
-ln -s ~/projects/tomfig/.inputrc ~/.inputrc
-ln -s ~/projects/tomfig/.ripgreprc ~/.ripgreprc
-ln -s ~/projects/tomfig/.vimrc ~/.vimrc
-ln -s ~/projects/tomfig/coc-settings.json ~/.vim/coc-settings.json
-ln -s ~/projects/tomfig/fusuma ~/.config
-ln -s ~/projects/tomfig/kitty ~/.config
-ln -s ~/projects/tomfig/alacritty/ ~/.config
+mkdir $HOME/.config/autostart
+mkdir $HOME/.vim
+ln -s $HOME/projects/tomfig/.bash $HOME/.bash
+ln -s $HOME/projects/tomfig/.bashrc $HOME/.bashrc
+ln -s $HOME/projects/tomfig/.bashrc $HOME/.bash_profile
+ln -s $HOME/projects/tomfig/.inputrc $HOME/.inputrc
+ln -s $HOME/projects/tomfig/.ripgreprc $HOME/.ripgreprc
+ln -s $HOME/projects/tomfig/.vimrc $HOME/.vimrc
+ln -s $HOME/projects/tomfig/coc-settings.json $HOME/.vim/coc-settings.json
+ln -s $HOME/projects/tomfig/fusuma $HOME/.config
+ln -s $HOME/projects/tomfig/kitty $HOME/.config
+ln -s $HOME/projects/tomfig/alacritty/ $HOME/.config
+
+vim -c 'PlugInstall|q'
+vim -c 'CocInstall -sync coc-json coc-html coc-tsserver coc-css coc-rls coc-yaml coc-highlight coc-emmet coc-snippets coc-lists|q'
 
 sudo gpasswd -a $USER input
 sudo gem install fusuma
 
-printf "[Desktop Entry]\nName=fusuma\nExec=fusuma -d\nType=Application" > ~/.config/autostart/fusuma.desktop
+printf "[Desktop Entry]\nName=fusuma\nExec=fusuma -d\nType=Application" > $HOME/.config/autostart/fusuma.desktop
 
 cargo install cargo-bump cargo-edit cargo-funnel
