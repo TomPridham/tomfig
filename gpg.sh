13c53d913edf9f6ec58d486116e40a7b0bccf1a9
Tom Pridham <pridham.tom@gmail.com>
2020-08-04 13:03:39 -0600

    fix read line and add description for gpg.sh

--- gpg.sh
+++ gpg.sh
@@ -1,7 +1,8 @@
 gpg --full-generate-key
 gpg --list-secret-keys --keyid-format LONG
 
-read "enter the gpg key id" GPGID
+echo "the key is of the form: 4096R/XXXXXXXXXXXXXXXX"
+read -p "enter the gpg key id: " GPGID
 
 gpg --armor --export GPGID | xclip -sel clip
 git config --global user.signingkey GPGID
