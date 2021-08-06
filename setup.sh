d05dedf58c7f4a92da497ddc4231527263bc6b73
Tom Pridham <pridham.tom@gmail.com>
2021-08-06 11:34:28 -0600

    replace cargo-bump with cargo-release

--- setup.sh
+++ setup.sh
@@ -35,7 +35,7 @@ ln -s $HOME/projects/tomfig/coc-settings.json $HOME/.vim/coc-settings.json
 vim -c 'PlugInstall|qa'
 vim -c 'CocInstall coc-json coc-html coc-tsserver coc-css coc-rust-analyzer coc-yaml coc-highlight coc-emmet coc-snippets coc-lists coc-vimtext|qa'
 
-cargo install cargo-bump cargo-edit cargo-funnel bat broot sccache
+cargo install cargo-release cargo-edit cargo-funnel bat broot sccache
 
 curl -sS https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/JetBrainsMono.zip -o /usr/share/fonts/jbmono_nf.zip
 echo unzip font file and move to ~/.fonts
