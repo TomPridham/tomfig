bde7f3101238e2fd9e3d3cbbe42720c6d41a6650
Tom Pridham <pridham.tom@gmail.com>
2021-08-06 17:10:26 -0600

    refactor: add br script to setup, hopefully fix font file, and make dirs

--- setup.sh
+++ setup.sh
@@ -18,24 +18,30 @@ rustup component add rls rust-analysis rust-src clippy rustfmt
 
 source ~/projects/tomfig/.bashrc
 
+mkdir -p $HOME/.bash
+mkdir -p $HOME/.config
+mkdir -p $HOME/.config/alacritty
+mkdir -p $HOME/.config/broot/launcher/bash
+mkdir -p $HOME/.config/fusuma
 mkdir -p $HOME/.vim
-[ ! -d "$HOME/.bash" ] && ln -s $HOME/projects/tomfig/.bash $HOME/.bash
-ln -s $HOME/projects/tomfig/.tmux.conf $HOME/.tmux.conf
-ln -s $HOME/projects/tomfig/.bashrc $HOME/.bashrc
+
+ln -s $HOME/projects/tomfig/.bash $HOME/.bash
 ln -s $HOME/projects/tomfig/.bashrc $HOME/.bash_profile
+ln -s $HOME/projects/tomfig/.bashrc $HOME/.bashrc
 ln -s $HOME/projects/tomfig/.inputrc $HOME/.inputrc
 ln -s $HOME/projects/tomfig/.ripgreprc $HOME/.ripgreprc
+ln -s $HOME/projects/tomfig/.tmux.conf $HOME/.tmux.conf
 ln -s $HOME/projects/tomfig/.vimrc $HOME/.vimrc
+ln -s $HOME/projects/tomfig/alacritty/ $HOME/.config
+ln -s $HOME/projects/tomfig/br.sh $HOME/.config/broot/launcher/bash/br
 ln -s $HOME/projects/tomfig/coc-settings.json $HOME/.vim/coc-settings.json
-[ ! -d "$HOME/.config/fusuma" ] && ln -s $HOME/projects/tomfig/fusuma $HOME/.config
-[ ! -d "$HOME/.config/" ] && ln -s $HOME/projects/tomfig/starship.toml $HOME/.config
-[ ! -d "$HOME/.config/kitty" ] && ln -s $HOME/projects/tomfig/kitty $HOME/.config
-[ ! -d "$HOME/.config/alacritty" ] && ln -s $HOME/projects/tomfig/alacritty/ $HOME/.config
+ln -s $HOME/projects/tomfig/fusuma $HOME/.config
+ln -s $HOME/projects/tomfig/starship.toml $HOME/.config
 
 vim -c 'PlugInstall|qa'
 vim -c 'CocInstall coc-json coc-html coc-tsserver coc-css coc-rust-analyzer coc-yaml coc-highlight coc-emmet coc-snippets coc-lists coc-vimtext|qa'
 
 cargo install cargo-release cargo-edit cargo-funnel bat broot sccache
 
-curl -sS https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/JetBrainsMono.zip -o /usr/share/fonts/jbmono_nf.zip
-echo unzip font file and move to ~/.fonts
+curl -LsS -N https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/JetBrainsMono.zip -o ~/.fonts/jbmono_nf.zip --create-dirs
+echo unzip font file
