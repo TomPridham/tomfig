9799e6f5e4971e493b7811fb9ae3f84178bcc1ea
Tom Pridham <pridham.tom@gmail.com>
2024-11-24 12:10:58 -0700

    fix: add other commands to fix permissions

--- gpg.sh
+++ gpg.sh
@@ -1,4 +1,7 @@
+# set correct permissions for gpg folders
 chown -R $(whoami) ~/.gnupg/
+find ~/.gnupg -type f -exec chmod 600 {} \;
+find ~/.gnupg -type d -exec chmod 700 {} \;
 
 gpg --default-new-key-algo rsa4096 --gen-key
 gpg --list-secret-keys --keyid-format LONG
